1a25ba2a7c5aacb3742449b88636c5d0b299de6e
sweetmilkitty <58770243+sweetmilkitty@users.noreply.github.com>
2020-02-13 17:33:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 `use strict`;
 {
-// const target1 =document.getElementById("target1")
-// const target2 =document.getElementById("target2")
-// const target3 =document.getElementById("target3")
-//
-// target1.addEventListener("click",()=>{
-//     target1.classList.toggle("circle");
-//     });
-//     target2.addEventListener("click",()=>{
-//         target2.classList.toggle("circle");
-//         });
-//         target3.addEventListener("click",()=>{
-//             target3.classList.toggle("circle");
-//             });
+
 const num=5;
 const winner =Math.floor(Math.random()*num);
 
@@ -21,10 +9,10 @@ const winner =Math.floor(Math.random()*num);
 for(let i =0; i<num; i++){
   const div =  document.createElement("div");
   div.classList.add("box");
-  // div.textContent =i;
+ 
 
   div.addEventListener("click",()=>{
-    // div.classList.toggle("circle");
+    
     if(i===winner){
       div.textContent ="Win!";
       div.classList.add("win");
